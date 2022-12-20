be6765f37623ad68497d28c1b829487abea047e6
overlordx <iriye@yahoo.com>
2022-12-19 22:55:55 -0500

    initial tests are working

--- main/onehotdb.py
+++ main/onehotdb.py
@@ -6,6 +6,8 @@ import mysql
 import pandas
 from mysql.connector import Error
 
+from main.dbtool import OneHotWords
+
 # edit configuration /etc/mysql/mysql.conf.d/mysqld.cnf to change bind-address/127.0.0.1 and port/3306
 # configuration edits require terminal commands: sudo service mysql stop, start, status
 
@@ -39,14 +41,13 @@ HOST = 'localhost'
 USER = 'bilbo'
 PASSWD = 'baggins'
 PORT = '3306'
-DB_NAME = 'dbtool'
-TABLE_NAME = 'dbtool'
+DB_NAME = 'onehot_db'
+TABLE_NAME = 'sentences'
 LEGAL_CHARACTERS = r"[^'a-zA-Z0-9\s\·\,\.\:\:\(\)\[\]\\\\]]"
 ILLEGAL_WORDS = ['True']
 LINK_KEY = 'link_key'
 FORBIDDEN_DATABASES = ['users']
 HTML_ESCAPE_TABLE = {
-    "&": "&amp;",
     '"': "&quot;",
     "'": "&apos;",
     ">": "&gt;",
@@ -59,25 +60,34 @@ HTML_ESCAPE_TABLE = {
 SELECT_STATEMENT = "SELECT {0} FROM {1} WHERE {2} = '{3}';"
 HASH_MOD = 1
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-ONEHOT_DB_NAME = 'onehot_words'
-ONE_HOT_WORD_TABLE_NAME = 'words'
-ONEHOT_KEY = 'sentence'
+ONEHOT_DB_NAME = 'onehot_tool'
+ONE_HOT_WORD_TABLE_NAME = 'test_dict'
+SENTENCE_KEY = 'sentence'
+# PKL_PATH = '/home/overlordx/PycharmProjects/SeminoleScraper/data/pkl'
+# DIR_PATH = r'/home/overlordx/PycharmProjects/SeminoleScraper/data/cases/seminole/10_7_2022'
 
 
 def get_onehot_encoded_string(_string, _hash_mod):
     word_indices = []
-    for word in _string:
-        OneHotWords().put(word)
+    words = _string.split(' ')
+    for word in words:
+        # OneHotWords().put(word)
         word_index = OneHotWords().get(word)
+        if word_index is None:
+            OneHotWords().put(LINK_KEY, word)
+            word_index = OneHotWords().get(word)
         word_indices.append(word_index)
+    encoded_string = ''
+    for index in word_indices:
+        encoded_string += '<' + index
     # hashed_string = hashlib.sha256(_string.encode('utf-8')).hexdigest(), 16 % 10 ** (8 * _hash_mod)
-    print('hash_string: ' + _string)
+    print('encoded_string: ' + encoded_string)
     # return str(hashed_string[0])
     # base64_bytes = _string.encode("ascii")
     # base64_bytes = base64.b64encode(_string)
     # base64_string = base64_bytes.decode("ascii")
     # return base64_string
-    return word_indices
+    return encoded_string
 
 
 def get_unhashed_string(_hashed_string, _hash_mod):
@@ -108,7 +118,7 @@ def get_clean_key(key):
     return clean_key
 
 
-class DBTool:
+class OneHotDB:
     def __init__(self, _database_name=None, _table_name=None):
         self.base_dir = ROOT_DIR.rsplit('/', 1)[0] + '/'
         # TODO: make table for key storage
@@ -145,7 +155,7 @@ class DBTool:
             connection.close()
         except Error as err:
             if err.errno == 1054 or str(err.args[1]).endswith('exists') or str(err.args[1]).__contains__('Duplicate'):
-                print('non-fatal error in dbtool._execute_mysql: ' + _mysql_statement)
+                print('non-fatal error in main._execute_mysql: ' + _mysql_statement)
             elif err.errno == 1064:
                 return 'mysql syntax error: ' +_mysql_statement
             else:
@@ -173,7 +183,7 @@ class DBTool:
                     port=port_num,
                     database=self.database_name
                 )
-            # print("dbtool._get_db_connection successful: " + db_name)
+            # print("main._get_db_connection successful: " + db_name)
         except Error as err:
             # err.errno(1049) is database not exists
             if err.errno == 1049:
@@ -231,7 +241,7 @@ class DBTool:
 
         # When you open_database self.table_name does not change
         # self.table is used in: _add_column, get, get_row_number, open_table, add_dataframe, and put
-        # The default value for self.table_name is dbtool
+        # The default value for self.table_name is main
         # Therefore, _table_name may be required for open_database
 
         # self.database_name = _database_name
@@ -286,6 +296,7 @@ class DBTool:
         return result
 
     def put(self, _link_key, _key_value=None, _value=None):
+        # convert _link_key to onehot_link_key
         # returns row_number of _link_key
         # 1 value: link_key
         # 2 value: clean_key, value
@@ -302,7 +313,7 @@ class DBTool:
         elif _value is None:
             # copies the value of link_key/_link_key to link_key/_key_value
             row_value = self.get(get_clean_key(_link_key))
-            columns = self._get_columns(get_clean_key(_link_key))
+            columns = self._get_columns()
             mysql_statement = "UPDATE {0} SET ({1}) = '{2}' WHERE {3} = '{4}';".format(self.table_name, LINK_KEY,
                                                                                        get_clean_key(_link_key),
                                                                                        LINK_KEY,
@@ -320,7 +331,29 @@ class DBTool:
         self._execute_mysql(mysql_statement)
         # print('put: ')
         result = self.get_row_number(get_clean_key(_link_key))
+
+
+
+
         return result
+
+    def put_onehot(self, _link_key, _string):
+        words = _string.strip().split(' ')
+        indices = []
+        for word in words:
+            word_result = OneHotWords().get(word)
+            if word_result is None:
+                OneHotWords().put(word)
+                word_result = OneHotWords().get(word)
+
+            index = str(word_result[0])
+            indices.append(index)
+        combined_indices = ''
+        for part in indices:
+            combined_indices = combined_indices + '&' + part
+            clean_link_key = get_clean_key(_link_key)
+        self.put(clean_link_key, SENTENCE_KEY, combined_indices)
+        print('put_onehot: ' + get_clean_key(_link_key) + combined_indices)
 
     def get_row_number(self, _link_key, _key=None):
         # select_template = '''SELECT * FROM seminole_main.seminole_main where link_key = "jurney";'''
@@ -363,7 +396,7 @@ class DBTool:
         return column_based_name
 
 
-class Pkl2Sql(DBTool):
+class Pkl2DB(OneHotDB):
     def __init__(self, _db_name, _dir_path):
         super().__init__(_db_name)
         self.dir_path = self.base_dir + get_clean_key(_dir_path)
@@ -387,8 +420,8 @@ class Pkl2Sql(DBTool):
             these_columns = get_onehot_encoded_string(this_pickle.columns, HASH_MOD)
             if not hashed_column_names.__contains__(these_columns):
                 hashed_column_names.append(these_columns)
-            dbtool = DBTool(self.database_name, these_columns)
-            dbtool.add_dataframe(this_pickle, 0)
+            this_onehot_db = OneHotDB(self.database_name, these_columns)
+            this_onehot_db.add_dataframe(this_pickle, 0)
         # print('add_pickles done!')
         return hashed_column_names
 
@@ -412,7 +445,7 @@ class Pkl2Sql(DBTool):
         return unencoded_table_names
 
 
-class Sql2Pkl(DBTool):
+class DB2Pkl(OneHotDB):
     # DB2Pkl takes a database_name and table_name and returns a pandas.DataFrame
     def __init__(self, _database_name, _table_name):
         super().__init__(_database_name, _table_name)
@@ -421,123 +454,66 @@ class Sql2Pkl(DBTool):
     def get_dataframe(self):
         print('get_dataframe done!')
 
-
-class OneHotWords(DBTool):
-    # mysql has a maximum number of 4096 columns per table
-    # Therefore, each sentence has a maximum number of words
-    def __init__(self):
-        super().__init__(ONEHOT_DB_NAME, ONE_HOT_WORD_TABLE_NAME)
-        # print('__init__ done!')
-
-    def get_index_combo(self, _string):
-        words = self.get_clean_string(_string)
-        words = words.split(' ')
-        word_indicies = []
-        for word in words:
-            word_indicies.append(self.get_row_number(word))
-        index_combo = ''
-        first = True
-        for word_index in word_indicies:
-            if not first:
-                index_combo += '&'
-            else:
-                first = False
-            index_combo += str(word_index)
-        return index_combo
-
-    def get_reconstituted_string(self, _index_combo):
-        indices = _index_combo.split('&')
-        words = []
-        for index in indices:
-            this_word = self.get_word_at_index(index)
-            if this_word is not None:
-                words.append(this_word)
-        reconstituted_string = ''
-        first = True
-        for word in words:
-            if not first:
-                reconstituted_string += ' '
-            else:
-                first = False
-            # # print('get_reconstituted_string word: ' + word)
-            reconstituted_string += str(word[0])
-        return reconstituted_string
-
-    def get_word_at_index(self, _index):
-        sql_statement = "SELECT link_key FROM {0} WHERE id = '{1}';".format(self.table_name, str(_index))
-        this_word = self._execute_mysql(sql_statement)
-        # print('get_word_at_index done!')
-        return this_word
-
-    def _put_word(self, _word, _word2=None, _word3=None):
-        clean_word = get_clean_key(_word)
-        super().put(LINK_KEY, clean_word)
-        # print('onehottool.put: ' + clean_word)
-
-    def _get_index(self, _word):
-        # This function returns the row number of _word in the onehot index
-        clean_word = self.get_clean_string(_word)
-        row_number = super().get_row_number(clean_word)
-        if row_number == '0':
-            self.put_word(clean_word)
-            row_number = super().get_row_number(clean_word)
-        # print('onehottool.get: ' + str(row_number))
-        return row_number
-
-    def load_dict(self, _dict_table_name, _dict_file_path):
-        self.open_table(_dict_table_name)
-        file_path = _dict_file_path
-        file = open(file_path, 'r')
-        text = file.read()
-        words = text.split('\n')
-        for word in words:
-            if word == 'ain''t':
-                clean_word = 'xyxxy'
-            clean_word = get_clean_key(word)
-            self._put_word(clean_word)
-
-
 def test_init():
-    dbtool = DBTool()
+    dbtool = OneHotDB()
     # print('test_init done!')
 
 
 def test_put():
-    dbtool = DBTool()
+    dbtool = OneHotDB()
     # make link_key/word_1
-    # dbtool.put('word')
-    # dbtool.put('put_1')
-    dbtool.put('xyzzy', 'failures', '543')
-    # dbtool.put('put_2')
-    # dbtool.put('put_2', 'failures', '345')
-    # dbtool.put('put_1', 'xyzzy')
+    dbtool.put_onehot('first_link_key', 'word is the bird')
+    dbtool.put_onehot('sef45', '''Business Insider
+A data scientist says Jack Dorsey told her Twitter was defenseless against a takeover by Elon Musk and the company should never have gone public
+Kate Duffy
+Mon, December 19, 2022 at 9:06 AM EST
+A collage of Elon Musk (left) and Twitter cofounder Jack Dorsey.
+Twitter owner Elon Musk and the platform's cofounder Jack Dorsey.Jim Watson/AFP via Getty Images
+Jack Dorsey said Twitter was defenseless against Elon Musk's acquisition, per a data scientist.
 
-    # dbtool.put('old_index_1', "old_index_2")
+Emily Gorcenski said she exchanged messages with Twitter cofounder about Musk's involvement.
+
+Dorsey said Twitter "should have never gone public" and that anyone could buy it, per Gorcenski.
+
+Jack Dorsey said Twitter had no defenses against Elon Musk acquiring the company in October, according to a data scientist and activist.
+
+Emily Gorcenski said she sent Dorsey a direct message via Twitter on November 4 about why he decided to hand the platform over to Musk. She said she told him: "It could have been so much more."
+
+Gorcenski, who shared images and screenshots with Insider to confirm her claims, told Dorsey he gave Twitter to a "charlatan" who was either "playing an act for fun" or chucking away the best of Twitter, adding that the employees who lost their jobs deserved better.
+
+Dorsey replied, saying these issues were expected to happen regardless of whether he was involved or not, according to Gorcenski's screenshots of the conversation posted on her Mastodon account.''')
+    # main.put('put_1')
+    # main.put('xyzzy', 'failures', '543')
+    # main.put('put_2')
+    # main.put('put_2', 'failures', '345')
+    # main.put('put_1', 'xyzzy')
+
+    # main.put('old_index_1', "old_index_2")
     # if link_key/word_1 does not exist then creates link_key/word_1 and renames link_key to word_1a
     # if link_key for word_1 does exist then this changes link_key/word_1 to word_1a
-    # dbtool.put('word_1', 'word_1a')
+    # main.put('word_1', 'word_1a')
     # sets key/word_1 to value/word_1a in row with link_key/word_1b
-    # dbtool.put('word_1', 'word_1a', 'word_1b')
+    # main.put('word_1', 'word_1a', 'word_1b')
     # print('test_put done!')
 
 
 def test_get():
-    dbtool = DBTool()
+    dbtool = OneHotDB()
     link_key_index = dbtool.get('put_2')
     print('index of put_2 from test_get(): ' + str(link_key_index))
-    # link_key_update_status = dbtool.get('put_1', 'reput_1')
+    # link_key_update_status = main.get('put_1', 'reput_1')
     # print(link_key_update_status)
 
 
 def test_get_row_count():
-    dbtool = DBTool('dbtool', 'dbtool')
+    dbtool = OneHotDB('main', 'main')
     row_count = dbtool.get_row_count()
     print('test_get_row_count: ' + str(row_count))
 
 
 def test_get_clean_key():
     KEY = "Instrument #"
-    dbtool = DBTool()
+    dbtool = OneHotDB()
     clean_key = get_clean_key(KEY)
     print('test_get_clean_key: ' + clean_key)
 
@@ -550,7 +526,7 @@ def test_add_data_frame():
     columns = ['username', 'password']
     data_frame.columns = columns
     # test the system
-    dbtool = DBTool()
+    dbtool = OneHotDB()
     column_based_name = dbtool.get_column_based_name(data_frame)
     dbtool.add_dataframe(data_frame)
     print('test_add_dataframe: ')
@@ -566,13 +542,13 @@ def test_pkl2sql():
     output_file_path = '../data/users.pkl'
     data_frame.to_pickle(output_file_path)
     # test the system
-    pkl2sql = Pkl2Sql(DB_NAME, 'data/')
+    pkl2sql = Pkl2DB(DB_NAME, 'data/')
     print('test_pkl2sql done!')
 
 
 def test_sql2pkl():
     # this class creates a pandas DataFrame
-    sql2pkl = Sql2Pkl(DB_NAME, TABLE_NAME)
+    sql2pkl = DB2Pkl(DB_NAME, TABLE_NAME)
     dataframe = sql2pkl.get_dataframe()
 
     print('test_sql2pkl done!')
@@ -589,4 +565,4 @@ if __name__ == '__main__':
     # test_pkl2sql()
     # test_sql2pkl()
     # the following line is not reached because of sys.exit() in main()
-    print("dbtool done!")
+    print("main done!")
