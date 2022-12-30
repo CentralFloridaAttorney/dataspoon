7f304e33e17805ee5fa625d9409d0b8bf4e015f4
CentralFloridaAttorney <attorney@centralfloridaattorney.net>
2022-12-29 19:23:57 -0500

    work on speedtest.py to fix put function

--- examples/speed_test.py
+++ examples/speed_test.py
@@ -6,8 +6,9 @@ from python.dataspoon.dbtool import DBTool
 # This example demonstrates the use of DBTool as a static tool
 # The following code inserts the current time in the column named 'time' in the row with a link_key 'xyzzy'
 start_time = datetime.datetime.now()
-DBTool().put('xyzzy', 'time', str(start_time))
-value = DBTool().get('xyzzy', 'time')
+dbtool = DBTool()
+dbtool.put('xyzzy', 'time', str(start_time))
+value = dbtool.get('xyzzy', 'time')
 end_time = datetime.datetime.now()
 # The following code prints the elapsed time for writing and reading the value
 print('start_time: ' + value)
